7fa5e4cae35e4bcaba0b495f1053f974101617b8
shwetaguptabhatnagar <shwetagupta806@gmail.com>
2021-09-21 17:52:16 +0530

    updated pom & apphooks

--- MyCucumberPOMSeries/src/test/java/AppHooks/ApplicationHooks.java
+++ MyCucumberPOMSeries/src/test/java/AppHooks/ApplicationHooks.java
@@ -1,5 +1,6 @@
 package AppHooks;
 
+import java.io.IOException;
 import java.util.Properties;
 
 import org.openqa.selenium.OutputType;
@@ -8,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 
 import com.qa.factory.DriverFactory;
 import com.qa.util.ConfigReader;
+import com.qa.util.PropertiesReader;
 
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -19,35 +21,58 @@ public class ApplicationHooks {
 	private WebDriver driver;
 	private ConfigReader configreader;
 	Properties prop;
-	
-	@Before(order=0)
-	public void getProperty()
-	{
-		configreader=new ConfigReader(); 
-		prop=configreader.init_prop();
+
+	@Before(order = 0)
+	public void getProperty() {
+		configreader = new ConfigReader();
+		prop = configreader.init_prop();
 	}
-	
-	@Before(order=1)
-	public void launchBrowser()
-	{
-		String browsername=prop.getProperty("browser");
-		driverFactory=new DriverFactory();
-		driver=driverFactory.init_driver(browsername);
+
+	@Before(order = 1)
+	public void launchBrowser() {
+
+		/*
+		 * java.io.InputStream is =
+		 * this.getClass().getResourceAsStream("my.properties"); java.util.Properties p
+		 * = new Properties(); try { p.load(is); } catch (IOException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); } String browsername =
+		 * p.getProperty("browser");
+		 * System.out.print("browsername in Hooks:"+browsername);
+		 */
+
+		// String browsername=System.getProperty("browser");
+
+		/*
+		 * PropertiesReader reader = null; try { reader = new
+		 * PropertiesReader("properties-from-pom.properties"); } catch (IOException e) {
+		 * // TODO Auto-generated catch block e.printStackTrace(); }
+		 */
+		
+		// String browsername = reader.getProperty("newbrowser");
+		 //System.out.print("browsername in Hooks:"+browsername);
+		 
+
+		//String browsername = prop.getProperty("browser");
+		//System.out.print("browsername in Hooks2:" + browsername);
+		 
+		 String browsername=System.getProperty("browser"); 
+		 System.out.print("browsername in Hooks:"+browsername);
+		 
+		driverFactory = new DriverFactory();
+		driver = driverFactory.init_driver(browsername);
 	}
-	
-	@After(order=0) // It will be executed last
-	public void quitBrowser()
-	{
+
+	@After(order = 0) // It will be executed last
+	public void quitBrowser() {
 		driver.quit();
 	}
-	@After(order=1) // It will be executed First
-	public void tearDown(Scenario sc)
-	{
-		if(sc.isFailed())
-		{//take screenshot
-		String screenshotname=sc.getName().replaceAll(" ", "_");
-		byte[] sourcePath=((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
-		sc.attach(sourcePath, "image/png", screenshotname);
+
+	@After(order = 1) // It will be executed First
+	public void tearDown(Scenario sc) {
+		if (sc.isFailed()) {// take screenshot
+			String screenshotname = sc.getName().replaceAll(" ", "_");
+			byte[] sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+			sc.attach(sourcePath, "image/png", screenshotname);
 		}
 	}
 }
